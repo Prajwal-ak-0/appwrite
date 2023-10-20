26576cf11500f27d6e361c38527d2ddd959d5ebf
Prajwal A K <118243416+Prajwal-ak-0@users.noreply.github.com>
2023-10-20 07:01:51 +0000

    Middleware Setted Up

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,46 @@
+"use client";
+
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage() {
+
+    const router=useRouter();
+
+    const logout = async () => {
+        try {
+            await axios.get("/api/users/logout");
+            toast.success("Logout Success");
+            router.push("/login");
+        } catch (error:any) {
+            console.log(error);
+            toast.error(error.response.data);
+        }
+    }
+
     return (
-        <div>
+        <div className="
+            min-h-screen
+            flex
+            flex-col
+            justify-center
+            items-center
+        " >
             Profile Page
+            <button
+             onClick={logout}
+             className="
+                bg-blue-500
+                hover:bg-blue-700
+                text-white
+                font-bold
+                py-2
+                px-4
+                rounded
+            " >
+                Logout
+            </button>
         </div>
     )
 }
